384a1b1c5132355554bd9ec7b988027d26ce9dd5
archserver <bryon.chase@yahoo.com>
2023-10-08 00:22:14 -0500

    chamber of commerce upload

---
+++ chamber/scripts/slideshow.js
@@ -0,0 +1,11 @@
+let index = 0;
+const images = document.querySelectorAll('.slideshow-images img');
+
+function slideshow() {
+    images[index].style.opacity = "0";
+    index = (index + 1) % images.length;
+    images[index].style.opacity = "1";
+    setTimeout(slideshow, 10000);  // Change the image every 3 seconds
+}
+
+window.onload = slideshow;
