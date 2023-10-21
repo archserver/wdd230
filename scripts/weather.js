d68151452a9ba6e9d3fafe239004d32048a9e398
archserver <bryon.chase@yahoo.com>
2023-10-21 00:03:09 -0500

    weather and windchill

---
+++ scripts/weather.js
@@ -0,0 +1,27 @@
+let zip = '77657';
+let apiKey = '89Qb9sTIShikhKdVo2lwaw==Uk9rHkxSxpCdzRBa';
+
+function updateWeatherInfo(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.api-ninjas.com/v1/weather?zip=' + zip);
+    xhr.setRequestHeader('X-Api-Key', apiKey);
+    xhr.responseType = 'json';
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            let result = xhr.response;
+            console.log(result);
+            document.getElementById('location').textContent = zip;
+            let fehrenheit = (result.temp * 9 / 5) + 32;
+            document.getElementById('temperature').textContent = fehrenheit;
+            document.getElementById('windspeed').textContent = result.wind_speed;
+            document.getElementById('winddirection').textContent = result.wind_degrees;
+        }
+        else{
+            console.error('Error: ', xhr.responseText);
+        }
+    };
+    xhr.send();
+} 
+
+updateWeatherInfo();
