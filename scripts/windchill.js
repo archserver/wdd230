2e982179a995c8d69743c26ace11827807cd9d4a
archserver <bryon.chase@yahoo.com>
2023-11-11 07:49:55 -0600

    updated wind chill

--- scripts/windchill.js
+++ scripts/windchill.js
@@ -7,7 +7,7 @@ function calculateWindChill() {
     if (!isNaN(windSpeed) && !isNaN(temperature)) {
         
         if(windSpeed > 3 && temperature <= 50){
-            windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+            windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
         }
     }
         document.getElementById('windchill').textContent = windChill === "N/A" ? windChill : windChill.toFixed(2) + " °F";
