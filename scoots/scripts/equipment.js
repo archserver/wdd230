c573e19ceda181e9e80e2f05dc59814a0b4bba8d
archserver <bryon.chase@yahoo.com>
2023-12-13 21:24:28 -0600

    final push

--- scoots/scripts/equipment.js
+++ scoots/scripts/equipment.js
@@ -1,6 +1,6 @@
 const equipmentTable = document.querySelector("#rentaltable table");
 const baseURL = 'https://archserver.github.io/wdd230/scoots/';
-const linksURL = 'https://archserver.github.io/wdd230/chamber/data/equipment.json';
+const linksURL = 'https://archserver.github.io/wdd230/scoots/data/equipment.json';
 
 
 async function getEquipment(){
@@ -9,8 +9,8 @@ async function getEquipment(){
         const response = await fetch(linksURL);
         if(response.ok){
             const data = await response.json();
-            console.table(data.rentals);
-            //displayEquipment(data.rentals);
+            //console.table(data.rentals);
+            displayEquipment(data.rentals);
         }
         else{
             throw Error(await response.text());
@@ -26,14 +26,16 @@ const displayEquipment = (equipmentlist) => {
    
     if(equipmentTable){
         //Table variable for building header
-        let tableHTML = `<thead> <tr> <th colspan="5" scope="colgroup">Rental Pricing</th> </tr>
+        let tableHTML = `<thead> <tr> <th colspan="6" scope="colgroup">Rental Pricing</th> </tr>
         <tr>
            <th scope="col"></th>
+           <th scope="col">Max</th>
            <th colspan="2" scope="col">Reservation</th>
            <th colspan="2" scope="col">Walk-In</th>
         </tr>
         <tr>
            <th scope="col"></th>
+           <th scope="col">Persons</th>
            <th scope="col">Half Day</th>
            <th scope="col">Full Day</th>
            <th scope="col">Half Day</th>
@@ -44,11 +46,12 @@ const displayEquipment = (equipmentlist) => {
         // for each Tier of equipment
         for(const tier in equipmentlist){
             if(equipmentlist.hasOwnProperty(tier)){
-                tableHTML += `<tbody><tr><th colspan="5">$tier.toUpperCase()}</th></tr>`;
+                tableHTML += `<tbody><tr><th colspan="6">${tier.toUpperCase()}</th></tr>`;
                 //for each item in each tier
                 equipmentlist[tier].forEach(item => {
                     tableHTML += `<tr>
                         <td>${item.name}</td>
+                        <td>${item.passangers}</td>
                         <td>${item.rhd}</td>
                         <td>${item.rfd}</td>
                         <td>${item.whd}</td>
